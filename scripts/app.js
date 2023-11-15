b94ffc1b73b227f497a449fc86f05aa64666d384
unknown <andre05fabiola@gmail.com>
2023-11-14 22:45:36 -0300

    pages y js

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+// Boton para hacer scroll hacia Arriba//
+
+const arriba = document.getElementById ("arriba")
+arriba.addEventListener("click" , () => {
+    window.scrollTo (0, 0);
+}); 
+
+buttonUp = document.getElementById ("arriba");
+
+window.onscroll = function () {
+    var scroll = document.documentElement.scrollTop; 
+    if (scroll > 700) {
+        buttonUp.style.transform = "scale(1)";
+    } else if (scroll < 500) {
+        buttonUp.style.transform = "scale(0)";
+    }
+}
